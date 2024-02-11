51c08af3712ed8d6f728822d725404dd07094250
Nicolas Petras <nicolaspetras@gmail.com>
2024-02-11 18:27:51 +0000

    day1: with crude solution for part 2

---
+++ adventofcode23/day1/calorie_counting.py
@@ -0,0 +1,41 @@
+"""Day 1: Calorie Counting"""
+
+
+def calculate_total_calories_from_input():
+    """
+    :return: list of elfs with the total calories for each one
+    """
+    with open("input", "r") as file:
+        sum_of_calories = 0     # sum for the current elf
+        elfs = []               # total calories for each elf
+        for line in file:
+            if line != "\n":
+                sum_of_calories += int(line)
+            else:
+                # new lines indicate the next elf is coming up
+                elfs.append(sum_of_calories)
+                sum_of_calories = 0
+        return elfs
+
+
+def part1_find_elf_with_most_calories():
+    """
+    :return: The number of calories held by the elf with the most snacks (calories)
+    """
+    part1_elfs = calculate_total_calories_from_input()
+    return max(part1_elfs)
+
+
+def part2_find_top_three_elfs():
+    part2_elfs = calculate_total_calories_from_input()
+    first_elf = max(part2_elfs)
+    part2_elfs.remove(first_elf)
+    second_elf = max(part2_elfs)
+    part2_elfs.remove(second_elf)
+    third_elf = max(part2_elfs)
+    return first_elf + second_elf + third_elf
+
+
+if __name__ == '__main__':
+    print(f"The top elf is carrying: {part1_find_elf_with_most_calories()} calories")
+    print(f"The top three elfs are carrying: {part2_find_top_three_elfs()} calories")
